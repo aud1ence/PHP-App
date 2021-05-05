aaf9c9769ae895b6a7ab9bc3c36420cec14d917b
aud1ence <daruma19051890@gmail.com>
2021-05-05 16:47:59 +0700

    done

---
+++ demo/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Thoi gian hien tai</title>
+    </head>
+    <body>
+        <h1> Bay gio la: <?php echo date_default_timezone_set('America/Los_Angeles'); ?></h1>
+    </body>
+</html>
